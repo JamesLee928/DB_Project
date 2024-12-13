0c4c72c20940097aececf3585a69a8d076fdbe7c
JamesLee928 <111703027@g.nccu.edu.tw>
2024-12-13 11:19:02 +0800

    Update models.py

--- DB_Project/app/models.py
+++ DB_Project/app/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     password = db.Column(db.String(200), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
+陳建銘好帥
 class Product(db.Model):
     id = db.Column(db.Integer, Primary_key=True)
     name = db.Column(db.String(100), nullable=False)
